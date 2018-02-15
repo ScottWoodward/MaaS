391e42b3dcd0c438007e52ba3d881c5a5a43b68e
Scott Woodward <scott.m.woodward@gmail.com>
2018-02-14 20:59:05 -0500

    Converted project to Spring Boot web project

---
+++ src/main/java/com/binaryphoenixstudios/maas/MaaSApplication.java
@@ -0,0 +1,20 @@
+package com.binaryphoenixstudios.maas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaaSApplication
+{
+	/**
+	 * Entry point for {@link SpringBootApplication}.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MaaSApplication.class, args);
+	}
+	
+	
+}
