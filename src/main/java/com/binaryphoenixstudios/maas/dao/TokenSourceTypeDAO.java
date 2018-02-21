89d2fea9703588b8bece00b1cf5192a9b31964ce
Scott Woodward <scott.m.woodward@gmail.com>
2018-02-20 21:20:53 -0500

    Added jpa framework for TokenSourceType

---
+++ src/main/java/com/binaryphoenixstudios/maas/dao/TokenSourceTypeDAO.java
@@ -0,0 +1,13 @@
+package com.binaryphoenixstudios.maas.dao;
+
+import com.binaryphoenixstudios.maas.bean.TokenSourceType;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface TokenSourceTypeDAO extends CrudRepository<TokenSourceType, Long>
+{
+	List<TokenSourceType> findAll();
+
+	TokenSourceType findByName();
+}
